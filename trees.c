2dcaa6d085278a69569065c1d87083592971c476
Elizabeth Rosso <lizprogrammer@yahoo.com>
2019-02-14 19:32:07 -0500

    coding challenge

---
+++ trees.c
@@ -0,0 +1,21 @@
+int main (void)
+{
+	printf("Hello\n");
+	int data[] = {5,8,2,9,4,1,3};
+	// testTree();
+	bruteForceTree();
+	
+
+}
+
+int seek(int data)
+{
+	// seek is going to scan the tree for the number sent
+	// returns 0 if the number is not found in the tree
+	// returns 1 if the number is found in the tree
+	BinaryNode* nav = root;
+	while("Not at the end of a branch or haven't found the data yet")
+	return 0;
+
+
+}
